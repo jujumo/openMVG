5aa0266e58946802b4043bbaf5c6bd169726ef0a
julien.morat <julien.morat@naverlabs.com>
2020-12-09 15:47:15 +0100

    debug intrinsics writing.

--- src/software/SfM/export/main_openMVG2kapture.cpp
+++ src/software/SfM/export/main_openMVG2kapture.cpp
@@ -37,91 +37,83 @@ bool CreateLineCameraFile(  const IndexT camera_id,
                             std::shared_ptr<openMVG::cameras::IntrinsicBase> intrinsic,
                             std::string & camera_linie)
 {
+  // kapture sensors.txt is formated like:
+  // sensor_device_id, name, sensor_type, [sensor_params]+
   std::stringstream came_line_ss;
+  const std::string SEP = ", ";
+  came_line_ss << camera_id << SEP << "cam_" << camera_id << SEP;
   EINTRINSIC current_type = intrinsic->getType();
   switch(current_type)
   {
     case PINHOLE_CAMERA:
       //OpenMVG's PINHOLE_CAMERA corresponds to kapture's SIMPLE_PINHOLE
-      //Parameters: f, cx, cy
+      //Parameters: w, h, f, cx, cy
       {
         std::shared_ptr<openMVG::cameras::Pinhole_Intrinsic> pinhole_intrinsic(
           dynamic_cast<openMVG::cameras::Pinhole_Intrinsic * >(intrinsic->clone()));
-
-        came_line_ss << camera_id << " " <<
-          "SIMPLE_PINHOLE" << " " <<
-          pinhole_intrinsic->w() << " " <<
-          pinhole_intrinsic->h() << " " <<
-          pinhole_intrinsic->focal() << " " <<
-          pinhole_intrinsic->principal_point().x() << " " <<
-          pinhole_intrinsic->principal_point().y() << "\n";
+        came_line_ss << "SIMPLE_PINHOLE" << SEP <<
+                        pinhole_intrinsic->w() << SEP <<
+                        pinhole_intrinsic->h() << SEP <<
+                        pinhole_intrinsic->focal() << SEP <<
+                        pinhole_intrinsic->principal_point().x() << SEP <<
+                        pinhole_intrinsic->principal_point().y() << std::endl;
       }
       break;
     case PINHOLE_CAMERA_RADIAL1:
       //OpenMVG's PINHOLE_CAMERA_RADIAL1 corresponds to kapture's SIMPLE_RADIAL
-      //Parameters: f, cx, cy, k1
+      //Parameters: w, h, f, cx, cy, k
       {
         std::shared_ptr<openMVG::cameras::Pinhole_Intrinsic_Radial_K1> pinhole_intrinsic_radial(
             dynamic_cast<openMVG::cameras::Pinhole_Intrinsic_Radial_K1 * >(intrinsic->clone()));
-
-        came_line_ss << camera_id << " " <<
-          "SIMPLE_RADIAL" << " " <<
-          pinhole_intrinsic_radial->w() << " " <<
-          pinhole_intrinsic_radial->h() << " " <<
-          pinhole_intrinsic_radial->focal() << " " <<
-          pinhole_intrinsic_radial->principal_point().x() << " " <<
-          pinhole_intrinsic_radial->principal_point().y() << " " <<
-          pinhole_intrinsic_radial->getParams().at(3) << "\n";   //k1
+        came_line_ss << "SIMPLE_RADIAL" << SEP <<
+                        pinhole_intrinsic_radial->w() << SEP <<
+                        pinhole_intrinsic_radial->h() << SEP <<
+                        pinhole_intrinsic_radial->focal() << SEP <<
+                        pinhole_intrinsic_radial->principal_point().x() << SEP <<
+                        pinhole_intrinsic_radial->principal_point().y() << SEP <<
+                        pinhole_intrinsic_radial->getParams().at(3) << std::endl;   //k1
       }
       break;
     case PINHOLE_CAMERA_RADIAL3:
       //OpenMVG's PINHOLE_CAMERA_RADIAL3 corresponds to kapture's FULL_OPENCV
-      //Parameters: fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
+      //Parameters: w, h, fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
       {
         std::shared_ptr<openMVG::cameras::Pinhole_Intrinsic_Radial_K3> pinhole_intrinsic_radial(
             dynamic_cast<openMVG::cameras::Pinhole_Intrinsic_Radial_K3 * >(intrinsic->clone()));
-
-        came_line_ss << camera_id << " " <<
-          "FULL_OPENCV" << " " <<
-          pinhole_intrinsic_radial->w() << " " <<
-          pinhole_intrinsic_radial->h() << " " <<
-          pinhole_intrinsic_radial->focal() << " " <<
-          pinhole_intrinsic_radial->focal() << " " <<
-          pinhole_intrinsic_radial->principal_point().x() << " " <<
-          pinhole_intrinsic_radial->principal_point().y() << " " <<
-          pinhole_intrinsic_radial->getParams().at(3) << " " << //k1
-          pinhole_intrinsic_radial->getParams().at(4) << " " << //k2
-          0.0 << " " << //p1
-          0.0 << " " << //p2
-          pinhole_intrinsic_radial->getParams().at(5) << " " << //k3
-          0.0 << " " << // k4
-          0.0 << " " << // k5
-          0.0 << "\n";  // k6
+        came_line_ss << "FULL_OPENCV" << SEP <<
+                        pinhole_intrinsic_radial->w() << SEP <<
+                        pinhole_intrinsic_radial->h() << SEP <<
+                        pinhole_intrinsic_radial->focal() << SEP <<
+                        pinhole_intrinsic_radial->focal() << SEP <<
+                        pinhole_intrinsic_radial->principal_point().x() << SEP <<
+                        pinhole_intrinsic_radial->principal_point().y() << SEP <<
+                        pinhole_intrinsic_radial->getParams().at(3) << SEP << //k1
+                        pinhole_intrinsic_radial->getParams().at(4) << SEP << //k2
+                        0.0 << SEP << //p1
+                        0.0 << SEP << //p2
+                        pinhole_intrinsic_radial->getParams().at(5) << SEP << //k3
+                        0.0 << SEP << // k4
+                        0.0 << SEP << // k5
+                        0.0 << std::endl;  // k6
       }
-      break;
-    case PINHOLE_CAMERA_BROWN:
-      std::cout << "PINHOLE_CAMERA_BROWN is not supported. Aborting ..." << std::endl;
-      return false;
       break;
     case PINHOLE_CAMERA_FISHEYE:
       //OpenMVG's PINHOLE_CAMERA_FISHEYE corresponds to kapture's OPENCV_FISHEYE
-      //Parameters: fx, fy, cx, cy, k1, k2, k3, k4
+      //Parameters: w, h, fx, fy, cx, cy, k1, k2, k3, k4
       {
         std::shared_ptr<openMVG::cameras::Pinhole_Intrinsic_Fisheye> pinhole_intrinsic_fisheye(
             dynamic_cast<openMVG::cameras::Pinhole_Intrinsic_Fisheye * >(intrinsic->clone()));
-
-        came_line_ss << camera_id << " " <<
-          "OPENCV_FISHEYE" << " " <<
-          pinhole_intrinsic_fisheye->w() << " " <<
-          pinhole_intrinsic_fisheye->h() << " " <<
-          pinhole_intrinsic_fisheye->focal() << " " <<
-          pinhole_intrinsic_fisheye->focal() << " " <<
-          pinhole_intrinsic_fisheye->principal_point().x() << " " <<
-          pinhole_intrinsic_fisheye->principal_point().y() << " " <<
-          pinhole_intrinsic_fisheye->getParams().at(3) << " " << //k1
-          pinhole_intrinsic_fisheye->getParams().at(4) << " " << //k2
-          pinhole_intrinsic_fisheye->getParams().at(5) << " " << //k3
-          pinhole_intrinsic_fisheye->getParams().at(6) << " " << "\n"; //k4
+        came_line_ss << "OPENCV_FISHEYE" << SEP <<
+                        pinhole_intrinsic_fisheye->w() << SEP <<
+                        pinhole_intrinsic_fisheye->h() << SEP <<
+                        pinhole_intrinsic_fisheye->focal() << SEP <<
+                        pinhole_intrinsic_fisheye->focal() << SEP <<
+                        pinhole_intrinsic_fisheye->principal_point().x() << SEP <<
+                        pinhole_intrinsic_fisheye->principal_point().y() << SEP <<
+                        pinhole_intrinsic_fisheye->getParams().at(3) << SEP << //k1
+                        pinhole_intrinsic_fisheye->getParams().at(4) << SEP << //k2
+                        pinhole_intrinsic_fisheye->getParams().at(5) << SEP << //k3
+                        pinhole_intrinsic_fisheye->getParams().at(6) << std::endl; //k4
       }
       break;
     default: std::cout << "Camera Type " << current_type << " is not supported. Aborting ..." << std::endl;
@@ -142,7 +134,7 @@ bool CreateSensorsFile( const SfM_Data & sfm_data,
   std::ofstream sensors_file( sSensorsFilename );
   if ( ! sensors_file )
   {
-    std::cerr << "Cannot write file" << sSensorsFilename << std::endl;
+    std::cerr << "Cannot write file " << sSensorsFilename << std::endl;
     return false;
   }
 
@@ -157,6 +149,7 @@ bool CreateSensorsFile( const SfM_Data & sfm_data,
     const IndexT camera_id = iter->first;
     std::shared_ptr<openMVG::cameras::IntrinsicBase> intrinsic = iter->second;
     std::string camera_line;
+
     if (CreateLineCameraFile(camera_id, intrinsic, camera_line))
     {
       camera_lines.push_back(camera_line);
@@ -199,16 +192,23 @@ bool CreateKaptureFolder( const SfM_Data & sfm_data,
 bool exportToKapture( const SfM_Data & sfm_data , const std::string & sOutDirectory  )
 {
   // Create output directory
-  bool bOk = false;
-  if ( !stlplus::is_folder( sOutDirectory ) )
-  {
-    std::cout << "\033[1;31mCreating directory:  " << sOutDirectory << "\033[0m\n";
-    stlplus::folder_create( sOutDirectory );
-    bOk = stlplus::is_folder( sOutDirectory );
-  }
-  else
-  {
-    bOk = true;
+  bool bOk = true;
+  const std::array< const std::string, 3 > output_dir_list = {
+      sOutDirectory, sOutDirectory + "/sensors", sOutDirectory + "/reconstruction"
+  };
+
+  for (auto output_dirpath : output_dir_list) {
+      //std::cout << "DEBUG : "<< output_dirpath << std::endl;
+      if ( !stlplus::is_folder( output_dirpath ) )
+      {
+        std::cout << "\033[1;31mCreating kapture directory in :  " << output_dirpath << "\033[0m\n";
+        stlplus::folder_create( output_dirpath );
+        bOk = stlplus::is_folder( output_dirpath );
+      }
+      else
+      {
+        bOk = bOk && true;
+      }
   }
 
   if ( !bOk )
@@ -217,10 +217,6 @@ bool exportToKapture( const SfM_Data & sfm_data , const std::string & sOutDirect
     return false;
   }
 
-
-  //const std::string sCamerasFilename = stlplus::create_filespec( sOutDirectory , "cameras.txt" );
-  //const std::string sImagesFilename = stlplus::create_filespec( sOutDirectory , "images.txt" );
-  //const std::string sPoints3DFilename = stlplus::create_filespec( sOutDirectory , "points3D.txt" );
   if ( ! CreateKaptureFolder( sfm_data , sOutDirectory) )
   {
     std::cerr << "There was an error exporting project" << std::endl;
@@ -260,11 +256,6 @@ int main( int argc , char ** argv )
 
     std::cerr << s << std::endl;
     return EXIT_FAILURE;
-  }
-
-  if ( !stlplus::folder_exists( sOutDir ) )
-  {
-    stlplus::folder_create( sOutDir );
   }
 
   // Read the input SfM scene
